1ac8070471b301a1da8ab1be4f99c551f111183c
Tony1120 <jliang1120tony@gmail.com>
2019-01-19 17:21:34 -0800

    added Orders comonent for the preparation of creating a seperate Orders Page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Order.module.css';
+const Order = (props) => {
+  return (
+    <div className = {classes.Order}>
+    	<p> Ingredents: salad (1)</p>
+    	<p> Ingredents: salad (1)</p>
+
+    	<p> Ingredents: salad (1)</p>
+    </div>
+  )
+}
+
+export default Order;
