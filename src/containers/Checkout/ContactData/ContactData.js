fe355acc6aae738127e35d5905c4c425bccdd9fe
Tony1120 <jliang1120tony@gmail.com>
2019-01-20 17:03:14 -0800

    dynamically display forms based on js config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,13 +5,61 @@ import axios from 'axios-orders'
 
 import Spinner from 'components/UI/Spinner/Spinner'
 import classes from './ContactData.module.css'
+import Input from 'components/UI/Input/Input'
+
 export default class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: ''
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'your Name'
+				},
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: ''
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Zip Code'
+				},
+				value: ''
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'your E-mail'
+				},
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest',displayValue: 'Fastest'},
+						{value: 'cheapest',displayValue: 'Cheapest'},
+					]
+				},
+				value: ''
+			}
 		},
 		loading: false
 	}
@@ -27,13 +75,8 @@ export default class ContactData extends Component {
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'tony',
-				address: {
-					street: 'xxx'
-				},
-				email: 'sss@gmail.com'
-			}
+
+
 		}
 		axios.post('/orders.json',order)
 			.then(response => {
@@ -54,14 +97,24 @@ export default class ContactData extends Component {
 
 
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			});
+		}
 		let form = (				
 				<form>
-					<input className = {classes.Input} type = "text" name = "name" placeholder = "your name" />
-					<input className = {classes.Input} type = "eamil" name = "email" placeholder = "your email" />
-					<input className = {classes.Input} type = "text" name = "street" placeholder = "your street" />
-					<input className = {classes.Input} type = "text" name = "postal" placeholder = "your PostCode" />					
-					<Button btnType = "Success" clicked = {this.orderHandler}> ORDER </Button>
+					{formElementsArray.map(formElement => (
+						<Input 
+							key = {formElement.id}
+							elementType = {formElement.config.elementType} 
+							elementConfig = {formElement.config.elementConfig} 
+							value = {formElement.config.value} />
 
+						))}				
+					<Button btnType = "Success" clicked = {this.orderHandler}> ORDER </Button>
 				</form> );
 
 		if (this.state.loading) {
