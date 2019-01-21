a3d3bbac40a766b118041a28026421ad7c024b95
Tony1120 <jliang1120tony@gmail.com>
2019-01-20 19:40:31 -0800

    the form is two-way binded with the user input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -95,6 +95,20 @@ export default class ContactData extends Component {
 			});
 	}
 
+	inputChangedHandler = (event, inputIdentifier	) => {
+		console.log(event.target.value);
+		const updatedOrderForm = {
+			...this.state.orderForm
+		};
+
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier]
+		};
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		this.setState({orderForm: updatedOrderForm});
+	}
+
 
 	render() {
 		const formElementsArray = [];
@@ -111,7 +125,8 @@ export default class ContactData extends Component {
 							key = {formElement.id}
 							elementType = {formElement.config.elementType} 
 							elementConfig = {formElement.config.elementConfig} 
-							value = {formElement.config.value} />
+							value = {formElement.config.value}
+							changed = {(event) => this.inputChangedHandler(event, formElement.id)} />
 
 						))}				
 					<Button btnType = "Success" clicked = {this.orderHandler}> ORDER </Button>
